9cdf1408e3695a11c9eeb75db216c3822e2ab8d4
Manoj <manojlds@gmail.com>
2017-08-12 00:36:42 +0530

    Service worker experiments

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'stacktoheap-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/css/syntax.css',
+  '/about/'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
