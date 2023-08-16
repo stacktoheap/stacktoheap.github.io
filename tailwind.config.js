7495ca6637afb289b471eac12f053db66f81c68a
Manoj Swaminathan <manoj.swaminathan@avalara.com>
2023-08-16 20:36:17 +0100

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
       './_drafts/**/*.html',
@@ -7,9 +9,37 @@ module.exports = {
       './*.md',
       './*.html',
     ],
+    darkMode: 'media',
     theme: {
       container: {
         center: true,
+      },
+      extend: {
+        typography: {
+          DEFAULT: {
+            css: {
+              pre: {
+                padding: "0",
+                color: "#1F2933",
+                backgroundColor: "#F3F3F3"
+              },
+              code: {
+                padding: "0.2em 0.4em",
+                backgroundColor: "#F3F3F3",
+                color: "#DD1144",
+                fontWeight: "400",
+                "border-radius": "0.25rem"
+              },
+              "code::before": false,
+              "code::after": false,
+              "blockquote p:first-of-type::before": false,
+              "blockquote p:last-of-type::after": false,
+            },
+          },
+        },
+        fontFamily: {
+          sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        },
       },
     },
     plugins: [
